022feabadab54bc05d42af0b7b183157d6d2b5ee
Bharat <58414297+Bharat-Runwal@users.noreply.github.com>
2021-06-30 23:39:47 +0530

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -25,6 +25,10 @@ from tensorflow.keras import backend as KB
 
 np.set_printoptions(edgeitems=3, linewidth=256)
 
+import time
+import scipy.ndimage as nd
+import numpy as np
+path = 'IMagenet/tiny-imagenet-200/'
 
 def get_session():
     gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
@@ -33,6 +37,61 @@ def get_session():
 
 tf.compat.v1.keras.backend.set_session(get_session())
 
+def get_id_dictionary():
+    id_dict = {}
+    for i, line in enumerate(open( path + 'wnids.txt', 'r')):
+        id_dict[line.replace('\n', '')] = i
+    return id_dict
+  
+def get_class_to_id_dict():
+    id_dict = get_id_dictionary()
+    all_classes = {}
+    result = {}
+    for i, line in enumerate(open( path + 'words.txt', 'r')):
+        n_id, word = line.split('\t')[:2]
+        all_classes[n_id] = word
+    for key, value in id_dict.items():
+        result[value] = (key, all_classes[key])      
+    return result
+
+def get_data(id_dict):
+    print('starting loading data')
+    train_data, test_data = [], []
+    train_labels, test_labels = [], []
+    t = time.time()
+    for key, value in id_dict.items():
+        train_data += [nd.imread( path + 'train/{}/images/{}_{}.JPEG'.format(key, key, str(i)), mode='RGB') for i in range(500)]
+        train_labels_ = np.array([[0]*200]*500)
+        train_labels_[:, value] = 1
+        train_labels += train_labels_.tolist()
+
+    for line in open( path + 'val/val_annotations.txt'):
+        img_name, class_id = line.split('\t')[:2]
+        test_data.append(nd.imread( path + 'val/images/{}'.format(img_name) ,mode='RGB'))
+        test_labels_ = np.array([[0]*200])
+        test_labels_[0, id_dict[class_id]] = 1
+        test_labels += test_labels_.tolist()
+
+    print('finished loading data, in {} seconds'.format(time.time() - t))
+    return np.array(train_data), np.array(train_labels), np.array(test_data), np.array(test_labels)
+
+def get_tiny_imagenet():
+  train_data, train_labels, test_data, test_labels = get_data(get_id_dictionary())
+  train_data= train_data.astype("float32")
+  test_data =test_data.astype("float32")
+  mean_image = np.mean(train_data, axis=0)
+
+  train_data -= mean_image
+  test_data -= mean_image
+  train_data /= 128.
+  test_data /= 128.
+
+  print( "train data shape: ",  train_data.shape )
+  print( "train label shape: ", train_labels.shape )
+  print( "test data shape: ",   test_data.shape )
+  print( "test_labels.shape: ", test_labels.shape )
+
+  return np.ascontiguousarray(train_data),np.ascontiguousarray(train_labels),np.ascontiguousarray(test_data),np.ascontiguousarray(test_labels),200
 
 def mnist():
     from tensorflow.examples.tutorials.mnist import input_data
@@ -322,7 +381,7 @@ def ResNet(config):
     pickle.dump(config, file)
     file.close()
 
-    data = cifar10()
+    data = get_tiny_imagenet()
     network = ResNetBuilder.MakeResNet(data[0].shape[1:], version, n, config)
     network.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(lr=0.001), metrics=['accuracy'])
     network.summary()
@@ -353,7 +412,7 @@ def LeNet(config):
     network.summary()
     print("All files will be available in:", mypath)
 
-    LeNetTrainer(network, mnist(), mypath, config)
+    LeNetTrainer(network, get_tiny_imagenet(), mypath, config)
     print("All files available in:", mypath)
     KB.clear_session()
 
@@ -375,8 +434,8 @@ def main(args):
     # config["wbits"] = 4
     # config["abits"] = 32
 
-    LeNet(config)
-    # ResNet(config)
+    # LeNet(config)
+    ResNet(config)
 
     return
 
